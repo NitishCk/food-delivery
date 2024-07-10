5e2047646b0c909d40879d3d00b354d1c45a4da2
Nitish CK <62250370+NitishCk@users.noreply.github.com>
2024-07-10 10:43:02 -0500

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = "https://food-delivery-backend-ie1y.onrender.com"
+export const url = 'https://food-delivery-backend-ie1y.onrender.com'
